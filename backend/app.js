053fdd6900fe6e7a80814016f1570dc93cc2d0b4
Bala Chinnaiah <97093618+BalaChinnaiah@users.noreply.github.com>
2026-08-23 14:57:44 +0530

    Added Vercel API To the CORS

--- backend/app.js
+++ backend/app.js
@@ -12,12 +12,10 @@ const app = express();
 
 // Middlewares
 
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true
-    })
-);
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 
 app.use(express.json());
 
